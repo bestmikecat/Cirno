9f16d07cc1e58183ab34dd287d9ce2307d04a891
adk <75924586+Adkimsm@users.noreply.github.com>
2026-03-20 23:21:57 +0800

    fix(cgroup): `Build.VERSION_CODES.BAKLAVA` not found

--- app/src/main/java/nep/timeline/cirno/utils/FrozenRW.java
+++ app/src/main/java/nep/timeline/cirno/utils/FrozenRW.java
@@ -3,7 +3,7 @@ package nep.timeline.cirno.utils;
 import android.os.Build;
 
 public class FrozenRW {
-    public static final String cgroupV2 = Build.VERSION.SDK_INT >= Build.VERSION_CODES.BAKLAVA ? "/sys/fs/cgroup/apps" : "/sys/fs/cgroup";
+    public static final String cgroupV2 = Build.VERSION.SDK_INT >= 36 ? "/sys/fs/cgroup/apps" : "/sys/fs/cgroup";
 
     public static void frozen(int uid, int pid) {
         RWUtils.writeFrozen(cgroupV2 + "/uid_" + uid + "/pid_" + pid + "/cgroup.freeze", 1);
