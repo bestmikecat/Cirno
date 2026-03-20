00d459c65fa9fa769d0bccc8a63c07671831d0da
adk <75924586+Adkimsm@users.noreply.github.com>
2026-03-20 22:52:03 +0800

    fix(cgroup): use correct directory on android 16

--- app/src/main/java/nep/timeline/cirno/utils/FrozenRW.java
+++ app/src/main/java/nep/timeline/cirno/utils/FrozenRW.java
@@ -1,7 +1,9 @@
 package nep.timeline.cirno.utils;
 
+import android.os.Build;
+
 public class FrozenRW {
-    public static final String cgroupV2 = "/sys/fs/cgroup";
+    public static final String cgroupV2 = Build.VERSION.SDK_INT >= Build.VERSION_CODES.BAKLAVA ? "/sys/fs/cgroup/apps" : "/sys/fs/cgroup";
 
     public static void frozen(int uid, int pid) {
         RWUtils.writeFrozen(cgroupV2 + "/uid_" + uid + "/pid_" + pid + "/cgroup.freeze", 1);
