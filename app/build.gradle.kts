43f74ab125f7d3bfde6469217f95f3ff51151634
adk <75924586+Adkimsm@users.noreply.github.com>
2026-04-05 10:04:26 +0800

    deps: remove test deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,10 +55,6 @@ dependencies {
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 
     val libsuVersion = "6.0.0"
     implementation("com.github.topjohnwu.libsu:core:$libsuVersion")
