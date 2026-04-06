cab85b26b1062c68c4338d5de9b5110fa5e2d05c
Adkimsm <adkinsm9277@gmail.com>
2026-04-06 10:31:58 +0800

    fix(network): try to fix on a16+

--- app/src/main/java/nep/timeline/cirno/services/NetworkManagementService.java
+++ app/src/main/java/nep/timeline/cirno/services/NetworkManagementService.java
@@ -1,10 +1,14 @@
 package nep.timeline.cirno.services;
 
 import java.lang.reflect.Array;
+import java.util.*;
+
+import android.os.Build;
 
 import de.robv.android.xposed.XposedHelpers;
 import nep.timeline.cirno.entity.AppRecord;
 import nep.timeline.cirno.log.Log;
+
 
 public class NetworkManagementService {
     public static volatile Object instance;
@@ -13,15 +17,25 @@ public class NetworkManagementService {
 
     public static void setInstance(Object obj, ClassLoader classLoader) {
         instance = obj;
+        if (Build.VERSION.SDK_INT >= 36) return;
         mNetdService = XposedHelpers.getObjectField(obj, "mNetdService");
         UidRangeParcel = XposedHelpers.findClass("android.net.UidRangeParcel", classLoader);
     }
 
     public static void socketDestroy(AppRecord appRecord) {
-        Object uidRangeParcels = Array.newInstance(UidRangeParcel, 1);
         int uid = appRecord.getUid();
-        Array.set(uidRangeParcels, 0, XposedHelpers.newInstance(UidRangeParcel, uid, uid));
-        XposedHelpers.callMethod(mNetdService, "socketDestroy", uidRangeParcels, new int[0]);
-        Log.d(appRecord.getPackageNameWithUser() + " 断开网络连接");
+        if (Build.VERSION.SDK_INT >= 36) {
+            Set<Integer> uidSet = new HashSet<>();
+            uidSet.add(uid)
+            XposedHelpers.callMethod(instance, "destroyLiveTcpSocketsByOwnerUids", uidSet);
+            Log.d(appRecord.getPackageNameWithUser() + " 断开网络连接");
+            return;
+        } else {
+            Array.set(uidRangeParcels, 0, XposedHelpers.newInstance(UidRangeParcel, uid, uid));
+            Object uidRangeParcels = Array.newInstance(UidRangeParcel, 1);
+            XposedHelpers.callMethod(mNetdService, "socketDestroy", uidRangeParcels, new int[0]);
+            Log.d(appRecord.getPackageNameWithUser() + " 断开网络连接");
+
+        }
     }
 }
