07f7b6e13469246fe38ac7a0e74d3fab29b011a3
Adkimsm <adkinsm9277@gmail.com>
2026-05-28 13:20:14 +0800

    fix(material): Restore settings dp import

--- app/src/main/java/nep/timeline/cirno/ui/page/material/MaterialSettingsPage.kt
+++ app/src/main/java/nep/timeline/cirno/ui/page/material/MaterialSettingsPage.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
