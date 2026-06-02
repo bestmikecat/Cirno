1e5fcfc25b497a17a828701da66266800a641e2a
Adkimsm <adkinsm9277@gmail.com>
2026-06-03 01:16:34 +0800

    fix(ui): Import root dialog spacing unit

--- app/src/main/java/nep/timeline/cirno/ui/dialog/RootDialog.kt
+++ app/src/main/java/nep/timeline/cirno/ui/dialog/RootDialog.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import nep.timeline.cirno.R
 import top.yukonga.miuix.kmp.basic.ButtonDefaults
 import top.yukonga.miuix.kmp.basic.TextButton as MiuixTextButton
