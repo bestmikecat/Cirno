366891b3886afe22da7037edb8d9838a105733d7
Adkimsm <adkinsm9277@gmail.com>
2026-06-07 00:57:20 +0800

    fix(addOn): check disable file to determine module enabled state

--- app/src/main/java/nep/timeline/cirno/ui/utils/AddOnStatusRepository.kt
+++ app/src/main/java/nep/timeline/cirno/ui/utils/AddOnStatusRepository.kt
@@ -4,10 +4,15 @@ import com.topjohnwu.superuser.io.SuFile
 
 object AddOnStatusRepository {
     private const val TOMB_STONE_MODULE_PROP = "/data/adb/modules/lib_tombstone/module.prop"
+    private const val TOMB_STONE_MODULE_DISABLE = "/data/adb/modules/lib_tombstone/disable"
 
     fun isAddOnEnabled(): Boolean {
         return try {
-            SuFile(TOMB_STONE_MODULE_PROP).exists()
+            val moduleProp = SuFile(TOMB_STONE_MODULE_PROP)
+            val disableFile = SuFile(TOMB_STONE_MODULE_DISABLE)
+            
+            // 模块存在 且 disable文件不存在
+            moduleProp.exists() && !disableFile.exists()
         } catch (_: Throwable) {
             false
         }
